d0a0887a72c7a2998e3c33be672a7c5fdea1d1f0
Michael Weedman <michael.weedman@gmail.com>
2020-01-23 12:53:51 -0700

    Completed through 12

--- main.js
+++ main.js
@@ -16,14 +16,14 @@ function main(){
   arr.push(45);
   arr.push(10); 
 
-  console.log(arr.get(0));
-  arr.pop(); 
-  arr.pop(); 
-  arr.pop(); 
-  arr.pop(); 
-  arr.pop(); 
-  arr.pop(); 
-  console.log('arr is ', arr);
+  //   console.log(arr.get(0));
+  //   arr.pop(); 
+  //   arr.pop(); 
+  //   arr.pop(); 
+  //   arr.pop(); 
+  //   arr.pop(); 
+  //   arr.pop(); 
+  //   console.log('arr is ', arr);
     
   console.log('arr is ', arr);
 }
@@ -35,8 +35,9 @@ main();
 const urlIfy = function(string) {
   return string.replace(/ /g, '%20');
 };
-console.log(urlIfy('tauhida parveen'));
-console.log(urlIfy('www.thinkful.com /tauh ida parv een'));
+console.log('5: ', urlIfy('www.thinkful.com /tauh ida parv een'));
+console.log('5: ', urlIfy('tauhida parveen'));
+
 //6 - Filtering an Array
 //test each number to see if less than 5, remove numbers matching true.
 const filter5 = function(array) {
@@ -48,7 +49,8 @@ const filter5 = function(array) {
   }
   return newArray;
 };
-console.log(filter5([4,6,-3,5,-2,1]));
+console.log('6: ', filter5([4,6,-3,5,-2,1]));
+
 //7 - Max sum in the Array
 // store greatestSum, add entire array over again.  anything that results in sum > greatestSum overwrites greatestSum
 const greatestSum = function(array) {
@@ -63,6 +65,7 @@ const greatestSum = function(array) {
 
 console.log('7: ', greatestSum([4,6,-3,5,-2,1]));
 console.log('7: ', greatestSum([0,2,6,-10,12,-12,18,10,-2]));
+
 //8 - Merge Arrays
 //2 sorted arrays
 const merge = function(array1, array2) {
@@ -93,17 +96,94 @@ const merge = function(array1, array2) {
   return newArray;
 };
 
-console.log(merge([1, 3, 6, 8, 11],[2, 3, 5, 8, 9, 10]));
+console.log('8: ', merge([1, 3, 6, 8, 11],[2, 3, 5, 8, 9, 10]));
+
 //9 - Remove Characters
 //deletes input character from string
 const deleteChars = function(string, toDelete) {
-  let charArray = toDelete.toCharacterArray();
-  // string.replace(toDelete,)
+  let charArray = [...toDelete];
+  charArray.forEach(char => { 
+    let replacing = new RegExp(char, 'gi');
+    string = string.replace(replacing, '');
+  });
+  
+  return string;
 };
 
-console.log(deleteChars('somestring', 'absd'));
+console.log('9: ', deleteChars('someString', 'absd'));
+
 //10 - Products
+//Products for remaining indices given index.
+const products = function(array) {
+  let productArray = [];
+  for (let i = 0; i < array.length; i++){
+    let whatever = 1;
+    for (let j = 0; j < array.length; j++) {
+      if (j !== i) {
+        whatever = whatever*array[j];
+      }
+    }
+    productArray.push(whatever);
+  }
+  return productArray;
+};
+
+console.log('10: ', products([1,3,9,4]));
 
 //11 - 2D Array
+const twoDSearch = function(array) {
+  let outputArray = array;
+  let columns = [];
+  let rows = [];
+  //mark where zeros occur
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[0].length; j++) {
+      if (array[i][j] === 0) {
+        rows.push(i);
+        columns.push(j);
+      }
+    } 
+  }
+  console.log('coordinates of zeros: ', rows, columns);
+
+  rows.forEach(row => {
+    for (let i = 0; i < outputArray[0].length; i++){
+      outputArray[row][i] = 0;
+    }
+  });
+  columns.forEach(column => {
+    for (let i = 0; i < outputArray.length; i++){
+      outputArray[i][column] = 0;
+    }
+  });
+
+  return outputArray;
+};
+
+console.log('11: ', twoDSearch([
+  [1,0,1,1,0],
+  [0,1,1,1,0],
+  [1,1,1,1,1],
+  [1,0,1,1,1],
+  [1,1,1,1,1]
+])
+);
 
 //12 - String Rotation
+const rotation = function(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  //substring of str1 === substring of str2 otherwise check next possible substring
+  //different lengths = false;
+  for (let i = 1; i < str1.length; i++) {
+    // console.log('substr1: ', str1.substring(0, i));
+    // console.log('substr2: ', str2.substring(str1.length - i, str1.length));
+    if (str1.substring(0, i) === str2.substring(str1.length - i, str1.length)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+console.log('12: ', rotation('arizona','izonaar'));
